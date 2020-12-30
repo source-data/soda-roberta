60954ba6be23c94cce895236bc6d3f97cb121838
Thomas Lemberger <thomas.lemberger@embo.org>
2020-12-30 19:47:19 +0100

    removed tokenizer name

--- lm/tokenize.py
+++ lm/tokenize.py
@@ -2,20 +2,18 @@ from argparse import ArgumentParser
 from tokenizers.implementations import ByteLevelBPETokenizer
 from tokenizers.processors import BertProcessing
 
-from . import DATASET, TOKENIZER_PATH, DEFAULT_TOKENIZER_NAME
+from . import DATASET, TOKENIZER_PATH
 
 
 def main():
     parser = ArgumentParser(description="Try the tokenizer.")
     parser.add_argument("text", help="Text to tokenize.")
-    parser.add_argument("--tokenizer_name", default=DEFAULT_TOKENIZER_NAME, help="The path to the tokenizer files.")
     args = parser.parse_args()
-    tokenizer_name = args.tokenizer_name
     text = args.text
 
     tokenizer = ByteLevelBPETokenizer(
-        f"{TOKENIZER_PATH}/{tokenizer_name}-vocab.json",
-        f"{TOKENIZER_PATH}/{tokenizer_name}-merges.txt",
+        f"{TOKENIZER_PATH}/vocab.json",
+        f"{TOKENIZER_PATH}/merges.txt",
     )
     tokenizer._tokenizer.post_processor = BertProcessing(
         ("</s>", tokenizer.token_to_id("</s>")),
@@ -24,7 +22,9 @@ def main():
     tokenizer.enable_truncation(max_length=512)
     tokenized = tokenizer.encode(text)
     print(tokenized.tokens)
+    print()
     print(tokenized.ids)
+    print()
     print(tokenized.offsets)
 
 
