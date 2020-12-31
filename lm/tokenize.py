4aea8bdee80e69c9914198799e2482c75220be9b
Thomas Lemberger <thomas.lemberge@embo.org>
2020-12-31 12:29:41 +0100

    cleaned up

--- lm/tokenize.py
+++ lm/tokenize.py
@@ -1,7 +1,6 @@
 from argparse import ArgumentParser
-# from tokenizers.implementations import ByteLevelBPETokenizer
+from tokenizers.implementations import ByteLevelBPETokenizer
 from tokenizers.processors import BertProcessing
-from transformers import RobertaTokenizerFast
 from common import TOKENIZER_PATH
 from common.config import config
 
@@ -12,16 +11,15 @@ def main():
     args = parser.parse_args()
     text = args.text
 
-    tokenizer = RobertaTokenizerFast.from_pretrained(TOKENIZER_PATH, max_len=config.max_length)
-    # tokenizer = ByteLevelBPETokenizer(
-    #     f"{TOKENIZER_PATH}/vocab.json",
-    #     f"{TOKENIZER_PATH}/merges.txt",
-    # )
+    tokenizer = ByteLevelBPETokenizer(
+        f"{TOKENIZER_PATH}/vocab.json",
+        f"{TOKENIZER_PATH}/merges.txt",
+    )
     tokenizer._tokenizer.post_processor = BertProcessing(
         ("</s>", tokenizer.token_to_id("</s>")),
         ("<s>", tokenizer.token_to_id("<s>")),
     )
-    tokenizer.enable_truncation(max_length=512)
+    tokenizer.enable_truncation(max_length=config.max_length)
     tokenized = tokenizer.encode(text)
     print(tokenized.tokens)
     print()
