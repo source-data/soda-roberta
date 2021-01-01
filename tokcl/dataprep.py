566e31fd0716439698a8be273e1bd35d35c1f7dd
Thomas Lemberger <thomas.lemberger@embo.org>
2021-01-01 12:20:26 +0100

    saving encoded dataset to json file

--- tokcl/dataprep.py
+++ tokcl/dataprep.py
@@ -1,7 +1,9 @@
 from pathlib import Path
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from xml.etree.ElementTree import parse, Element, ElementTree
+import json
 import shutil
+from datetime import datetime
 from tokenizers import Encoding, ByteLevelBPETokenizer
 from .encoder import XMLEncoder
 from .xmlcode import (
@@ -17,16 +19,16 @@ class SDPreparator:
     Args:
         source_dir_path (Path):
             The path to the source xml files.
-        dest_dir_path (Path):
-            The path to the desitnation directory where the encoded labeled examples should be saved.
+        dest_path (Path):
+            The path of the file where the encoded labeled examples should be saved.
         tokenizer (ByteLevelBPETokenizer):
             The pre-trained tokenizer to use for processing the inner text.
         code_map (CodeMap):
             The XML-to-code constraints mapping label codes to specific combinations of tag name and attribute values.
     """
-    def __init__(self, source_dir_path: Path, dest_dir_path: Path, tokenizer: ByteLevelBPETokenizer, code_map: CodeMap):
+    def __init__(self, source_dir_path: Path, dest_path: Path, tokenizer: ByteLevelBPETokenizer, code_map: CodeMap):
         self.source_dir_path = source_dir_path
-        self.dest_dir_path = dest_dir_path
+        self.dest_path = dest_path
         self.ec = XMLEncoder(code_map)
         self.tokenizer = tokenizer
 
@@ -44,18 +46,22 @@ class SDPreparator:
             with filepath.open() as f:
                 xml_example: ElementTree = parse(f)
             xml_example: Element = xml_example.getroot()
-            labeled_token = self._encode_example(xml_example)
-            labeled_examples.append(labeled_token)
-            self._save(filepath, labeled_token)
+            tokenized, token_level_label_ids = self._encode_example(xml_example)
+            labeled_examples.append({
+                'tokenized': tokenized,
+                'label_ids': token_level_label_ids
+            })
+            # self._save_individual_example(filepath, labeled_token)
+        self._save_json(labeled_examples)
         return labeled_examples
 
     def _encode_example(self, xml: Element) -> List:
-        character_level_label_ids = self.ec.encode(xml)
-        character_level_label_ids = [c if c is not None else -100 for c in character_level_label_ids]
+        xml_encoded = self.ec.encode(xml)
+        character_level_label_ids = [c if c is not None else -100 for c in xml_encoded['label_ids']]
         inner_text = innertext(xml)
         tokenized = self.tokenizer.encode(inner_text)  # uses Whitespace as pre_processor
-        labeled_token = self._align_labels(tokenized, character_level_label_ids)
-        return labeled_token
+        token_level_label_ids = self._align_labels(tokenized, character_level_label_ids)
+        return tokenized, token_level_label_ids
 
     def _align_labels(self, tokenized: Encoding, character_level_label_ids: list) -> Tuple[List[int], List[str], List[int]]:
         token_level_label_ids = []
@@ -63,7 +69,19 @@ class SDPreparator:
             start, end = tokenized.offsets[i]
             label_id = character_level_label_ids[end - 1]  # better to take last character of token since firt can be special character for space; but see # https://huggingface.co/transformers/custom_datasets.html#tok-ner
             token_level_label_ids.append(label_id)
-        return (token_level_label_ids, tokenized.tokens, tokenized.ids)
+        return token_level_label_ids
+
+    def _save_json(self, labeled_examples: Dict):
+        for examples in labeled_examples:
+            j = {
+                'version': datetime.now().isoformat(),
+                'dataset': {
+                    'tokens': examples['tokenized'].tokens,
+                    'label_ids':  examples['label_ids'],
+                }
+            }
+        with self.dest_path.open('w', encoding='utf-8') as f:
+            json.dump(j, f)
 
     def _save(self, source_filepath: Path, labeled_token: Tuple[List[int], List[str], List[int]]):
         basename = source_filepath.stem
@@ -91,8 +109,7 @@ def self_test():
     path.mkdir()
     source_path = path / 'source'
     source_path.mkdir()
-    dest_path = path / 'dest'
-    dest_path.mkdir()
+    dest_path = path / 'dataset.json'
     source_file_path = source_path / 'example.xml'
     source_file_path.write_text(example)
     expected_label_codes = [-100, -100, -100, -100, -100, -100, 2, 2, 2, 2, -100, -100, 4, -100, -100, -100]
@@ -101,14 +118,15 @@ def self_test():
         data_prep = SDPreparator(source_path, dest_path, tokenizer, EntityTypeCodeMap)
         labeled_examples = data_prep.run()
         print("\nLabel codes: ")
-        print(labeled_examples[0][0])
-        assert labeled_examples[0][0] == expected_label_codes
+        print(labeled_examples[0]['label_ids'])
+        assert labeled_examples[0]['label_ids'] == expected_label_codes
         print('\nTokens')
-        print(labeled_examples[0][1])
-        assert labeled_examples[0][1] == expected_tokens
-        dest_file_path = dest_path / 'example.txt'
-        print(f"\nContent of saved file ({dest_file_path}):")
-        print(dest_file_path.read_text())
+        print(labeled_examples[0]['tokenized'].tokens)
+        assert labeled_examples[0]['tokenized'].tokens == expected_tokens
+        print(f"\nContent of saved file ({dest_path}):")
+        with dest_path.open() as f:
+            j = json.load(f)
+        print(json.dumps(j, indent=2))
     finally:
         shutil.rmtree('/tmp/test_dataprep/')
         print("cleaned up and removed /tmp/test_corpus")
