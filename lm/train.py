872ab0ceb7b6419e53f4ca1bcc3470e08e3a6cea
Thomas Lemberger <thomas.lemberge@embo.org>
2021-01-04 14:04:21 +0100

    experimenting

--- lm/train.py
+++ lm/train.py
@@ -33,7 +33,7 @@ print(f"Evaluating on {len(eval_dataset)} examples.")
 
 config = RobertaConfig(
     vocab_size=config.vocab_size,
-    # max_position_embeddings=config.max_length + 2,  # max_length + 2 for start/end token?
+    max_position_embeddings=config.max_length + 2,  # max_length + 2 for start/end token?
     num_attention_heads=12,
     num_hidden_layers=6,
     type_vocab_size=1,
