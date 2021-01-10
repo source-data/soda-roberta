bc87c01584c65923bee81a8c18368009690e0deb
Thomas Lemberger <thomas.lemberger@embo.org>
2021-01-10 10:17:47 +0100

    many corrections for overalping tokens

--- tokcl/dataprep.py
+++ tokcl/dataprep.py
@@ -21,7 +21,7 @@ from common.config import config
 class Preparator:
     """Processes source xml documents into examples that can be used in a token classification task.
     It tokenizes the text with the provided tokenizer. 
-    The XML is used to generate labels according to the provided CodeMaps.
+    The XML is used to generate labels according to the provided CodeMap.
     The datset is then split into train, eval, and test set and saved into json line files.
 
     Args:
@@ -77,28 +77,115 @@ class Preparator:
             with filepath.open() as f:
                 xml_example: ElementTree = parse(f)
             xml_example: Element = xml_example.getroot()
-            labeled_example = self._encode_example(xml_example)
-            labeled_examples.append(labeled_example)
+            tokenized, token_level_labels = self._encode_example(xml_example)
+            labeled_examples.append({
+                'tokenized': tokenized,
+                'label_ids': token_level_labels
+            })
         split_examples = self._split(labeled_examples)
         self._save_json(split_examples)
         return labeled_examples
 
     def _encode_example(self, xml: Element) -> Tuple[BatchEncoding, Dict]:
-        xml_encoder = XMLEncoder(xml, self.tokenizer)
+        xml_encoder = XMLEncoder(xml)
+        inner_text = innertext(xml_encoder.element)
+        tokenized: BatchEncoding = self.tokenizer(
+            inner_text,
+            max_length=self.max_length,
+            truncation=True,
+            return_offsets_mapping=True,
+            add_special_tokens=True
+        )
         token_level_labels = {}
         for code_map in self.code_maps:
-            xml_encoded = xml_encoder.encode(
-                code_map,
-                max_length=self.max_length,
-                truncation=True,
-                add_special_tokens=True
-            )
-            input_ids = xml_encoded['token_input_ids']
-            token_level_labels[code_map.name] = xml_encoded['token_labels']
-        return {
-            'input_ids': input_ids,
-            'label_ids': token_level_labels
-        }
+            xml_encoded = xml_encoder.encode(code_map)
+            labels = self._align_labels(tokenized, xml_encoded, code_map, inner_text)
+            token_level_labels[code_map.name] = labels
+        return tokenized, token_level_labels
+
+    def _align_labels(self, tokenized: Encoding, xml_encoded: Dict, code_map: CodeMap, inner_text) -> List[int]:
+        num_tokens = len(tokenized.input_ids)
+        # prefill with outside of entity label 'O' using IOB2 scheme
+        token_level_labels = ['O'] * num_tokens
+        # tokenizer may have truncated the example
+        last_token_start, last_token_end = tokenized.offset_mapping[-2]  # -2 because the last token is </s> with offsets (0,0) by convention
+        # get the character-level start end end of the xml element and try to map to tokens
+        for element_start, element_end in xml_encoded['offsets']:
+            # check we are still within the truncated example
+            if (element_start <= last_token_start) & (element_end < last_token_end):
+                code = xml_encoded['label_ids'][element_start]  # element_end would give the same, maybe check with assert
+                assert xml_encoded['label_ids'][element_start] == xml_encoded['label_ids'][element_end - 1], f"{xml_encoded['label_ids'][element_start:element_end]}\n{element_start, element_end}"
+                start_token_idx = self._char_to_token(element_start, inner_text, tokenized)
+                end_token_idx = self._char_to_token(element_end, inner_text, tokenized)
+                # sanity check
+                try:
+                    assert start_token_idx is not None, f"\n\nproblem with start token None."
+                    assert end_token_idx is not None, f"\n\nproblem with end token None."
+                except Exception:
+                    import pdb; pdb.set_trace()
+                if (start_token_idx == end_token_idx):
+                    # In addition, the tokenizer may generate a token that is actually spanning an element boundary.
+                    # Recursive tokenized in the XMLEncode is NOT a solution as it will force learning on atypical tokenization that will not be
+                    # representative of tokenization of free text. It actually destroys prediction :-(
+                    # But empty element cannot not correspond to any token
+                    start, end = tokenized.offset_mapping[end_token_idx]
+                    if (start <= element_start) and (end > element_end):
+                        print(f"WARNING: token overlaps element boundary {code_map.constraints[code]['tag']} at position {element_end} in '{inner_text[start-10:start]}>>>{inner_text[start:element_end]}^{inner_text[element_end:end]}...<<<{inner_text[end:end+10]}'")
+                        # if next token outside of an element, will be labeled; if part of next element, labelig will be overriden
+                        end_token_idx += 1 if end_token_idx <= num_tokens else num_tokens
+                    else:
+                        print(f"WARNING: emtpy element {code_map.constraints[code]['tag']}? at position {element_start, element_end} in >>>{inner_text[element_start:element_start+50]}...<<<")
+                prefix = "B"  # for B-egining token according to IOB2 scheme
+                if code_map.mode == 'whole_entity':  # label all the tokens corresponding to the xml element
+                    for token_idx in range(start_token_idx, end_token_idx):
+                        label = self._int_code_to_iob2_label(prefix, code, code_map)
+                        token_level_labels[token_idx] = label
+                        prefix = "I"  # for subsequet I-nside tokens
+                elif code_map.mode == 'boundary_start' and (start_token_idx != end_token_idx):  # label the B-egining of non-empty elements
+                    label = self._int_code_to_iob2_label(prefix, code, code_map)
+                    token_level_labels[start_token_idx] = label
+            else:
+                # the last token has been reached, no point scanning further elemnts
+                break
+        return token_level_labels
+
+    @staticmethod
+    def _char_to_token(element_pos, inner_text, tokenized):
+        # Nasty: because of RobertaTokenizer's behavior with spaces, 
+        # a space before a word is included in token. When this happens across xml element boundary, 
+        # the character at the boundary position is a space and is included in the next or previous token outside the element.
+        # In addition, BatchEncoding.char_to_token() will return None if the token is a single space
+        # proper token will be found only from next or previous character, respectively
+        # This gymnastic is to try to circumven this.
+        pos = element_pos
+        _, last_pos = tokenized.offset_mapping[-2]  # end of last non special token
+        if pos >= len(inner_text):
+            token_idx = len(tokenized.input_ids)
+            return token_idx
+        elif inner_text[pos] != ' ':  # usual case, not in a space, all fine
+            token_idx = tokenized.char_to_token(pos)
+            return token_idx
+        while (inner_text[pos] == ' ') and (pos < last_pos - 1): pos += 1  # scanning for non space on the right
+        if inner_text[pos] == ' ':  # we are still in a run of space and at the end of the string!
+            token_idx = len(tokenized.input_ids) - 1
+        else:
+            # __.token    is tokenized into two single spaces plus one .token (dot is spacial character produced by RobertaTokenizer)
+            #    ^        need to scan until non space character
+            # 5           element_start = 5
+            # 5678        pos = 8 after scanning
+            # 234         actual start_token_idx 2, first non space token is 4, tokens 2 and 3 are single spaces
+            num_single_space_tokens = pos - 1 - element_pos
+            try:
+                token_idx = tokenized.char_to_token(pos) - num_single_space_tokens
+            except Exception:
+                import pdb; pdb.set_trace()
+        return token_idx
+
+    @staticmethod
+    def _int_code_to_iob2_label(prefix: str, code: int, code_map: CodeMap) -> str:
+        label = code_map.constraints[code]['label']
+        iob2_label = f"{prefix}-{label}"
+        return iob2_label
 
     def _split(self, examples: List) -> Dict:
         shuffle(examples)
@@ -121,7 +208,12 @@ class Preparator:
             filepath = self.dest_dir_path / f"{subset}.jsonl"
             with filepath.open('a', encoding='utf-8') as f:  # mode 'a' to append lines
                 for example in examples:
-                    f.write(f"{json.dumps(example)}\n")
+                    j = {
+                        'tokens': example['tokenized'].tokens(),
+                        'input_ids': example['tokenized'].input_ids,
+                        'label_ids':  example['label_ids']
+                    }
+                    f.write(f"{json.dumps(j)}\n")
 
     def verify(self):
         filepaths = self.dest_dir_path.glob("**/*.jsonl")
@@ -129,8 +221,9 @@ class Preparator:
             with p.open() as f:
                 for n, line in enumerate(f):
                     j = json.loads(line)
-                    L = len(j['input_ids'])
+                    L = len(j['tokens'])
                     assert L <= self.max_length, f"Length verification: error line {n} in {p} with {len(j['tokens'])} tokens > {self.max_length}."
+                    assert len(j['input_ids']) == L, f"mismatch in number of tokens and input_ids: error line {n} in {p}"
                     for k, label_ids in j['label_ids'].items():
                         assert len(label_ids) == L, f"mismatch in number of tokens and {k} label_ids: error line {n} in {p}"
         print("\nLength verification: OK!")
@@ -138,10 +231,9 @@ class Preparator:
 
 
 def self_test():
-    # example = "<xml><a>  </a>.</xml>"
+    # example = '<sd-panel> of an adult <sd-tag type="gene">Prox1</sd-tag>-<sd-tag type="gene">Cre</sd-tag><sd-tag type="gene">ER</sd-tag>T2;<sd-tag type="gene">Ilk</sd-tag>+/+ <sd-tag type="organism">mouse</sd-tag> (referred to as "Adult Control")</sd-panel>'
     example = "<xml>Here <sd-panel>it is<sd-tag role='reporter'> </sd-tag>: <i>nested <sd-tag role='reporter'>in</sd-tag> <sd-tag category='entity' type='gene' role='intervention'>Creb-1</sd-tag> with some <sd-tag type='protein' role='assayed'>tail</sd-tag></i>. End </sd-panel>."
     example += ' 1 2 3 4 5 6 7 8 9 0' + '</xml>'  # to test truncation
-    tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base')
     path = Path('/tmp/test_dataprep')
     path.mkdir()
     source_path = path / 'source'
@@ -149,35 +241,78 @@ def self_test():
     dest_dir_path = path / 'dataset'
     source_file_path = source_path / 'example.xml'
     source_file_path.write_text(example)
-    max_length = 22  # in token!
+    max_length = 20  # in token!
     expected_tokens = [
-        '<s>', 'Here', 'Ġ', 'it', 'Ġis', 'Ġ', ':', 'Ġ', 'n', 'ested', 'Ġ', 'in', 'Ġ', 'Cre', 'b', '-', '1', 'Ġwith', 'Ġsome', 'Ġ', 'tail', '</s>'
+        '<s>',
+        'Here', 'Ġit', 'Ġis', 'Ġ:', 'Ġnested', 'Ġin',
+        'ĠCre', 'b', '-', '1',
+        'Ġwith', 'Ġsome',
+        'Ġtail',
+        '.', 'ĠEnd', 'Ġ.',
+        'Ġ1', 'Ġ2',
+        '</s>'
     ]
     expected_label_codes = {
         'entity_types': [
-            # '<s>', 'Here', 'Ġ', 'it', 'Ġis', 'Ġ', ':', 'Ġ', 'n', 'ested', 'Ġ', 'in', 'Ġ', 'Cre',        'b',          '-',          '1',          'Ġwith', 'Ġsome', 'Ġ', 'tail',       '</s>'
-              'O',   'O',    'O', 'O',  'O',   'O', 'O' ,'O', 'O', 'O',     'O',  'O', 'O', 'B-GENEPROD', 'I-GENEPROD', 'I-GENEPROD', 'I-GENEPROD', 'O',     'O',     'O', 'B-GENEPROD', 'O'
+            'O',
+            'O', 'O', 'O', 'O', 'O', 'O',
+            'B-GENEPROD', 'I-GENEPROD', 'I-GENEPROD', 'I-GENEPROD',
+            'O', 'O',
+            'B-GENEPROD',
+            'O', 'O', 'O',
+            'O',
+            'O',
+            'O'
         ],
         'geneprod_roles': [
-              'O',  'O',     'O', 'O',  'O',   'O', 'O', 'O', 'O', 'O',     'O',  'O', 'O', 'B-CONTROLLED_VAR', 'I-CONTROLLED_VAR', 'I-CONTROLLED_VAR', 'I-CONTROLLED_VAR', 'O', 'O', 'O', 'B-MEASURED_VAR', 'O'
+            'O',
+            'O', 'O', 'O', 'O', 'O', 'O',
+            'B-CONTROLLED_VAR', 'I-CONTROLLED_VAR', 'I-CONTROLLED_VAR', 'I-CONTROLLED_VAR',
+            'O', 'O',
+            'B-MEASURED_VAR',
+            'O', 'O', 'O',
+            'O',
+            'O',
+            'O'
+        ],
+        'boring': [
+            'O',
+            'O', 'O', 'O', 'O', 'O', 'B-BORING',
+            'O', 'O', 'O', 'O',
+            'O', 'O',
+            'O',
+            'O', 'O', 'O',
+            'O',
+            'O',
+            'O'
         ],
         'panel_start': [
-              'O', 'O',      'O', 'B-PANEL_START', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'
+            'O',
+            'O', 'B-PANEL_START', 'O', 'O', 'O', 'O',
+            'O', 'O', 'O', 'O',
+            'O', 'O',
+            'O',
+            'O', 'O', 'O',
+            'O',
+            'O',
+            'O'
         ]
     }
     try:
+        tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base')
         data_prep = Preparator(source_path, dest_dir_path, tokenizer, [sd.ENTITY_TYPES, sd.GENEPROD_ROLES, sd.PANELIZATION], max_length=max_length)
         labeled_examples = data_prep.run()
         print("\nXML examples:")
         print(example)
-        print('\nTokens')
-        tokens = [tokenizer.convert_ids_to_tokens(id) for id in labeled_examples[0]['input_ids']]
-        print(tokens)
         print("\nLabel codes: ")
-        print(labeled_examples[0]['label_ids'])
-
+        for i in range(len(labeled_examples[0]['tokenized'].input_ids)):
+            token = labeled_examples[0]['tokenized'].tokens()[i]
+            input_id = labeled_examples[0]['tokenized'].input_ids[i]
+            decoded = tokenizer.decode(input_id)
+            label_ids = "\t".join([labels[i] for labels in labeled_examples[0]['label_ids'].values()])
+            print(f"{token}\t{decoded}\t{label_ids}")
         labeled_example_label_ids = labeled_examples[0]['label_ids']
-        assert tokens == expected_tokens, tokens
+        assert labeled_examples[0]['tokenized'].tokens() == expected_tokens, labeled_examples[0]['tokenized'].tokens()
         assert labeled_example_label_ids['entity_types'] == expected_label_codes['entity_types'], labeled_example_label_ids['entity_types']
         assert labeled_example_label_ids['geneprod_roles'] == expected_label_codes['geneprod_roles'], labeled_example_label_ids['geneprod_roles']
         assert labeled_example_label_ids['panel_start'] == expected_label_codes['panel_start'], labeled_example_label_ids['panel_start']
@@ -188,7 +323,7 @@ def self_test():
             with filepath.open() as f:
                 for line in f:
                     j = json.loads(line)
-                    print(json.dumps(j, indent=2))
+                    print(json.dumps(j))
     finally:
         shutil.rmtree('/tmp/test_dataprep/')
         print("cleaned up and removed /tmp/test_corpus")
--- tokcl/encoder.py
+++ tokcl/encoder.py
@@ -1,102 +1,72 @@
 from xml.etree.ElementTree import Element, fromstring, tostring
 from typing import List
-from transformers import RobertaTokenizerFast, BatchEncoding
 from .xmlcode import CodeMap, SourceDataCodes as sd
 from common.utils import innertext
 
 
 class XMLEncoder:
     """Encodes a XML object in a list of label ids based on the XML-to-label mapping provided by the supplied CodeMap.
-    Both character- and token-level codes are produced.
 
     Args:
-        element (Element):
-            The XML Element to encode
-        tokenizer (RobertaTokenizerFats): the tokenizer to be usd to recursively tokenized the xml.
+        code_map (CodeMap):
+            The CodeMap object that maps label codes (int) to specic combinations of tag name and attribute values.
     """
-
-    def __init__(self, element: Element, tokenizer: RobertaTokenizerFast):
+    def __init__(self, element: Element):
         self.element = element
-        self.tokenizer = tokenizer
 
-    def encode(self, code_map: CodeMap, add_special_tokens: bool = True, max_length: int = None, truncation: bool = False):
-        """Encodes an Element into a list of character-level label codes (int) as well as token-level IOB labels.
-        Character-level positions that are not assigned with any code are filled with None.
-        THe XML tree is traversed and tokenized recursively so that tokenization cannot produce tokens that would span across xml elements.
-
+    def encode(self, code_map: CodeMap):
+        """Encodes an Element into a list of character-level label codes (int).
+        Positions that are not assigned with any code are filled with None.
+        THe XML tree is traversed recursively and as soon as an element satistifes to one constraints provided in code_map, 
+        the entire span of the element is assigned this code.
         To visualize run:
             python -m tokcl.encoder
         without any arguments.
 
         Args:
-            code_map (CodeMap):
-                The CodeMap object that maps label codes (int) to specic combinations of tag name and attribute values.
-            add_special_tokens (bool): to add begining and end of sentece bos_token_id and eos_token_id
-            max_length: the maximum length in terms of token. There is no effect if truncation is not set to True. 
-            truncation (bool): whether to truncate tokenized element
+            element (Element):
+                The XML Element to encode
 
         Returns:
             (Dict[List[int], List[Tuple[int, int]]]):
                 A dictionary with:
                    - 'label_ids' (List): the list of label ids
-                   - 'token_input_ids' (List[int]): the list of token ids after recursive tokenization.
-                   - 'token_labels' (List[str]): the list of IOB labels in register with the list of tokens.
-                   - 'xml_str (str): the stringifyed xml for reference and debugging
+                   - 'offsets' (Tuple[int, int]): the offsets indicating the start and end postition of each labeled element
+                   - 'xml' (str): the xml as string for reference and degbuging
         """
         self.code_map = code_map
-        char_level_codes, token_input_ids, token_labels = self._encode(self.element)
-        if max_length and truncation:
-            stop = max_length
-            if add_special_tokens:
-                stop -= 2  # such that len(token_indput_ids[:stop]) + 2 special tokens = max_length
-            token_input_ids = token_input_ids[:stop]
-            token_labels = token_labels[:stop]
-        if add_special_tokens:
-            token_input_ids = [self.tokenizer.bos_token_id] + token_input_ids + [self.tokenizer.eos_token_id]
-            token_labels = ["O"] + token_labels + ["O"]
-        labels_and_offsets = {
-            'char_level_codes': char_level_codes,
-            'token_input_ids': token_input_ids,
-            'token_labels': token_labels,
-            'xml_str': tostring(self.element).decode('utf-8')  # tostring() returns bytes
-        }
+        encoded, offsets, _ = self._encode(self.element)
+        labels_and_offsets = {'label_ids': encoded, 'offsets': offsets, 'xml': tostring(self.element)}
+        for start, end in offsets:
+            assert encoded[start] == encoded[end-1], f"{encoded[start:end]}\n{start}, {end},\n{innertext(self.element)}\n{innertext(self.element)[start:end]}\n{tostring(self.element)}"
         return labels_and_offsets
 
-    def _encode(self, element: Element, code: int = None, prefix: str = '') -> List[int]:
+    def _encode(self, element: Element, pos: int = 0) -> List[int]:
         text_element = element.text or ''
         L_text = len(text_element)
         text_tail = element.tail or ''
         L_tail = len(text_tail)
-        token_ids = self._tokenize(text_element)
-        token_ids_tail = self._tokenize(text_tail)
-        num_tokens = len(token_ids)
-        token_labels = ["O"] * num_tokens
-        if not code:
-            code = self._get_code(element)
-            prefix = "B"  # for B-egining token according to IOB2 scheme
+        code = self._get_code(element)
+        inner_text = innertext(element)
+        L_inner_text = len(inner_text)
+        # print(f"{'•' * pos}{inner_text}<{element.tag}, {pos}, {pos + L_inner_text}, tail={L_tail}>")
         if code:
-            char_level_codes = [code] * L_text
-            if self.code_map.mode == 'whole_entity':  # label all the tokens corresponding to the xml element
-                for token_idx in range(num_tokens):
-                    token_labels[token_idx] = self._int_code_to_iob2_label(prefix, code, self.code_map)
-                    prefix = "I"  # for subsequet I-nside tokens
-            elif self.code_map.mode == 'boundary_start':  # label only the B-egining
-                if token_labels:
-                    token_labels[0] = self._int_code_to_iob2_label(prefix, code, self.code_map)
-                    prefix = ""
-                    code = None  # to prevent inheriting code in children elements
+            # as soon as an element corresponds to one of the code, the code is proagated on the whole length of the element and its tail
+            encoded = [code] * L_inner_text
+            offsets = [(pos, pos + L_inner_text)]
+            pos += L_inner_text
         else:
-            char_level_codes = [None] * L_text
-        # check child elements
-        for child in element:
-            child_char_level_codes, child_token_ids, child_token_labels = self._encode(child, code=code, prefix=prefix)
-            char_level_codes += child_char_level_codes
-            token_ids += child_token_ids
-            token_labels += child_token_labels
-        token_ids = token_ids + token_ids_tail
-        char_level_codes = char_level_codes + [None] * L_tail
-        token_labels = token_labels + ["O"] * len(token_ids_tail)
-        return char_level_codes, token_ids, token_labels
+            encoded = [None] * L_text
+            offsets = []
+            pos += L_text
+            # check child elements
+            for child in element:
+                child_encoded, child_offsets, pos = self._encode(child, pos=pos)
+                encoded += child_encoded
+                offsets += child_offsets
+        encoded = encoded + [None] * L_tail
+        pos += L_tail
+        return encoded, offsets, pos
 
     def _get_code(self, element: Element) -> int:
         for code, constraint in self.code_map.constraints.items():
@@ -112,43 +82,26 @@ class XMLEncoder:
         # the element does not match any of the constraints
         return None
 
-    @staticmethod
-    def _int_code_to_iob2_label(prefix: str, code: int, code_map: CodeMap) -> str:
-        label = code_map.constraints[code]['label']
-        iob2_label = f"{prefix}-{label}"
-        return iob2_label
-
-    def _tokenize(self, text: str) -> BatchEncoding:
-        return self.tokenizer(
-            text,
-            truncation=False,
-            add_special_tokens=False
-        ).input_ids
-
 
 def demo():
-    # example = "<xml><span>Here</span> <sd-panel> it is: <i>nested in <sd-tag category='entity' type='gene' role='reporter'>Creb-1</sd-tag> with some <sd-tag type='protein' role='assayed'>tail</sd-tag></i>. End</sd-panel>.</xml>"
     example = "<xml>Here <sd-panel><p>it is<sd-tag role='reporter'> </sd-tag>: <i>nested <sd-tag role='reporter'>in</sd-tag> <sd-tag category='entity' type='gene' role='intervention'>Creb-1</sd-tag> with some <sd-tag type='protein' role='assayed'>tail</sd-tag></i>. End </p></sd-panel>."
     example += ' 1 2 3 4 5' + '</xml>'
     xml = fromstring(example)
-    tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base')
-    xe = XMLEncoder(xml, tokenizer)
-    inner_text = innertext(xe.element)
-    encoded = xe.encode(sd.ENTITY_TYPES, max_length=22, truncation=True, add_special_tokens=True)
-    assert len(encoded['char_level_codes']) == len(inner_text)
-    text4viz = ''.join([c + '  ' for c in inner_text])
+    inner_text = innertext(xml)
+    xe = XMLEncoder(xml)
+    encoded = xe.encode(sd.ENTITY_TYPES)
+    assert len(encoded['label_ids']) == len(inner_text)
+    text = ''.join([c + '  ' for c in inner_text])
     print("\nExample xml:\n")
     print(example)
-    print("\nReduced inner text and aligned features with codes:\n")
-    print(text4viz)
+    print("\nInner text and features with codes:\n")
+    print(text)
     trace = []
-    trace = [f"{c:02}" if c is not None else '__' for c in encoded['char_level_codes']]
+    trace = [f"{c:02}" if c is not None else '__' for c in encoded['label_ids']]
     print(f"{' '.join(trace)}")
-    for idx in range(len(encoded['token_input_ids'])):
-        token_id = encoded['token_input_ids'][idx]
-        token = xe.tokenizer.convert_ids_to_tokens(token_id)
-        token_label = encoded['token_labels'][idx]
-        print(f"{idx}:\t{token_id}\t{token}\t{token_label}")
+    print(f"\nOffsets of the labeled elements with their code:")
+    for start, end in encoded['offsets']:
+        print(f"'{inner_text[start:end]}': start={start}, end={end}, with codes: {encoded['label_ids'][start:end]}")
 
 
 if __name__ == '__main__':
