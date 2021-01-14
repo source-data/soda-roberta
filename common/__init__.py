656b8e6dd30427c1421d1d83bca14dbf968cc3d0
Thomas Lemberger <thomas.lemberger@embo.org>
2021-01-14 18:39:54 +0100

    updating env

--- common/__init__.py
+++ common/__init__.py
@@ -3,9 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 LM_DATASET = os.getenv('LM_DATASET')
-NER_DATASET = os.getenv('NER_DATASET')
-TOKENIZER_PATH = os.getenv('TOKENIZER_PATH')
-DEFAULT_TOKENIZER_NAME = os.getenv('DEFAULT_TOKENIZER_NAME')
 LM_MODEL_PATH = os.getenv('LM_MODEL_PATH')
-NER_MODEL_PATH = os.getenv('NER_MODEL_PATH')
-HUGGINGFACE_CACHE = os.getenv('HUGGINGFACE_CACHE')
+TOKENIZER_PATH = os.getenv('TOKENIZER_PATH')
+TOKCL_DATASET = os.getenv('TOKCL_DATASET')
+TOKCL_MODEL_PATH = os.getenv('TOKCL_MODEL_PATH')
+CACHE = os.getenv('CACHE')
