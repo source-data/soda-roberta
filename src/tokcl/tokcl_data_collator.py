5956934f58af3a7aadba5cbd63748f8d13c4871b
Thomas Lemberger <thomas.lemberger@embo.org>
2021-01-22 23:49:12 +0100

    intermediate

--- src/tokcl/tokcl_data_collator.py
+++ src/tokcl/tokcl_data_collator.py
@@ -96,25 +96,25 @@ class DataCollatorForMaskedTokenClassification:
         # convert dict of list of lists into ditc of tensors
         batch = {k: torch.tensor(v, dtype=torch.int64) for k, v in batch.items()}
         # stochastically mask input ids according to tag_mask
-        batch["input_ids"], batch["labels"] = self.tag_mask_tokens(batch["input_ids"], batch["labels"], batch["tag_mask"])
+        batch["input_ids"], batch["labels"] = self.tag_mask_tokens(batch["input_ids"], batch["labels"], batch["tag_mask"].bool())
         # remove tak_mask from match as it would be rejected by model.forward()
         batch.pop("tag_mask")
         return batch
 
     def tag_mask_tokens(self, inputs: torch.Tensor, targets: torch.Tensor, mask: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         """Masks the input as specified by the tag mask prepared by the loader"""
-        inputs = inputs.clone()  # not sure if necessart; might be safer to avoid changing input features when provided as tensor
+        inputs = inputs.clone()  # not sure if necessary; might be safer to avoid changing input features when provided as tensor
         if self.select_labels:
             targets = targets.clone()
         # create and initialize the probability matrix for masking to zeros
         probability_matrix = torch.zeros_like(inputs, dtype=torch.float64)
-        # update in-place probability to the set mlm_probability value where mask is true
-        probability_matrix.masked_fill_(mask.bool(), value=self.masking_probability)
+        # update in-place to the masking_probability where mask is true
+        probability_matrix.masked_fill_(mask, value=self.masking_probability)
         # use the probability at each position to randomly mask or not
         masked_indices = torch.bernoulli(probability_matrix).bool()
         # replace input_ids by the mask token id at position that need to be masked
         inputs[masked_indices] = self.tokenizer.mask_token_id
         if self.select_labels:
-            # only labels at the masked positions will be used for calculating the loss
-            targets[~masked_indices] = -100
+            # only labels at the makred position (irrespective of whether they are masked) will be used for calculating the loss
+            targets[~mask] = -100
         return inputs, targets
