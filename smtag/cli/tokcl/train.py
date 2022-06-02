87f0a99d972670ee128be9b3309448a829c5008b
jabreu <jorge.abreu@embo.org>
2022-06-02 13:52:07 +0200

    Check larger learning rate

--- smtag/cli/tokcl/train.py
+++ smtag/cli/tokcl/train.py
@@ -69,8 +69,8 @@ if __name__ == "__main__":
         # "EMBO/BioMegatron345mUncased": "EMBO/BioMegatron345mUncased"
     }
 
-    learning_rate = [0.0002] # [0.0001, 0.00005]
-    lr_scheduler = ["linear", "cosine"] # ["constant", "linear", "cosine"]
+    learning_rate = [0.000075] # [0.0001, 0.00005]
+    lr_scheduler = ["constant"] # ["constant", "linear", "cosine"]
     dropout_list = [0.1] # Dropout is very similar. We will leave 0.1
     per_device_train_batch_size = 16
     hidden_size_multiple_values = [64] # [32, 64] # 16 learns too slow and 128 does not even learn with masked
