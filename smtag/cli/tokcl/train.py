cbfe6e7fc0c09b91b2534881d8ef6c3afa0e2239
jabreu <jorge.abreu@embo.org>
2022-06-09 14:43:15 +0200

    Training TOKCL script clean

--- smtag/cli/tokcl/train.py
+++ smtag/cli/tokcl/train.py
@@ -46,113 +46,33 @@ if __name__ == "__main__":
                         help="""If set to true, it will use the test dataset to do an
                         inference and return the performance of the model in the
                         test dataset.""")
-    parser.add_argument("--test_results_file",
-                        default="./test_results_benchmark.json",
-                        help="""JSON file with the results of the model.""")
 
-    # tasks = ["NER", "GENEPROD_ROLES", "SMALL_MOL_ROLES", "BORING", "PANELIZATION"]
-    tasks = ["NER"]
-    models = {
-        "EMBO/bio-lm": "EMBO/bio-lm",
-        "EMBO/bert-base-cased": "bert-base-cased",
-        "EMBO/bert-base-uncased": "bert-base-uncased",
-        # # "EMBO/bert-large-cased": "bert-large-cased",
-        # # "EMBO/bert-large-uncased": "bert-large-uncased",
-        "EMBO/roberta-base": "roberta-base",
-        # # "EMBO/roberta-large": "roberta-large",
-        "EMBO/biobert-base-cased": "dmis-lab/biobert-base-cased-v1.2",
-        # # "EMBO/biobert-large-cased": "dmis-lab/biobert-large-cased-v1.1",
-        "EMBO/PubMedBERT-base-uncased-abstract-fulltext": "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext",
-        "EMBO/PubMedBERT-base-uncased-abstract": "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract",
-        "EMBO/BioMegatron345mCased": "EMBO/BioMegatron345mCased",
-        "EMBO/BioMegatron345mUncased": "EMBO/BioMegatron345mUncased",
-    }
+    training_args, args = parser.parse_args_into_dataclasses()
+    no_cache = args.no_cache
+    loader_path = args.loader_path
+    task = args.task
+    data_config_name = args.data_config_name
+    data_dir = args.data_dir
+    from_pretrained = args.from_pretrained
+    model_type = args.model_type
+    hidden_size_multiple = int(args.hidden_size_multiple)
+    masked_data_collator = bool(args.masked_data_collator)
+    dropout = float(args.dropout)
+    do_test = bool(args.do_test)
+    tokenizer_name = from_pretrained if args.tokenizer is None else args.tokenizer
 
-    learning_rate = [0.000075] # [0.0001, 0.00005]
-    lr_scheduler = ["linear"] # ["constant", "linear", "cosine"]
-    dropout_list = [0.1] # Dropout is very similar. We will leave 0.1
-    per_device_train_batch_size = 8
-    hidden_size_multiple_values = [64] # [32, 64] # 16 learns too slow and 128 does not even learn with masked
-    prediction_loss_only = False
-    evaluation_strategy = "epoch"
-    eval_steps = 1
-    save_total_limit = 500
-    num_train_epochs = 10
-    save_strategy = "epoch"
-    save_steps = 500
-    seed = 42
-    load_best_model_at_end = True
-    metric_best_model = "f1"
-    greater_is_better = True
-    report_to = "tensorboard"
-    push_to_hub = False
-    hub_strategy = "checkpoint"
-    overwrite_output_dir = True
-    test_results_file = "benchmarking_results.pkl"
-    model_type = "Autoencoder"
-    do_test = True
+    trainer = TrainModel(
+        training_args=TrainingArgumentsTOKCL(**training_args),
+        loader_path=loader_path,
+        task=task,
+        from_pretrained=from_pretrained,
+        tokenizer_name=tokenizer_name,
+        model_type=model_type,
+        do_test=do_test,
+        dropout=dropout,
+        hidden_size_multiple=hidden_size_multiple,
+        masked_data_collator=masked_data_collator,
+    )
 
-    for model in models.keys():
-        dataset_to_load = "drAbreu/sd-nlp-2" if models[model] in ["EMBO/bio-lm",
-                                                                  "roberta-base",
-                                                                  "roberta-large"] else "EMBO/sd-nlp-non-tokenized"
-        tokenizer_name = "roberta-base" if models[model] in ["EMBO/bio-lm"] else models[model]
-        for task in tasks:
-            for lr in learning_rate:
-                for lr_schedule in lr_scheduler:
-                    for masked in [True]: #[False, True]:
-                        for dr in dropout_list:
-                            for hidden_size in hidden_size_multiple_values:
-                                hub_model_id = f"EMBO/{model}_{task}"
-                                output_dir = f"./tokcl_models/EMBO_{model}_{task}"
-                                test_results_file = "benchmarking_results.pkl"
-                                # Here I should add the parameters for the base model.
-                                # That would give a much better control over the benchmark
-                                training_args = {
-                                    "learning_rate": lr,
-                                    "lr_scheduler_type": lr_schedule,
-                                    "per_device_eval_batch_size": 64,
-                                    "per_device_train_batch_size": per_device_train_batch_size,
-                                    "prediction_loss_only": False,
-                                    "evaluation_strategy": "epoch",
-                                    "eval_steps": 1,
-                                    "save_total_limit": 500,
-                                    "num_train_epochs": num_train_epochs,
-                                    "save_strategy": "epoch",
-                                    "save_steps": 1,
-                                    "seed": 42,
-                                    "load_best_model_at_end": True,
-                                    "metric_for_best_model": "f1",
-                                    "greater_is_better": True,
-                                    "report_to": "tensorboard",
-                                    "push_to_hub": False,
-                                    "hub_strategy": "checkpoint",
-                                    "overwrite_output_dir": True,
-                                    "hub_model_id": hub_model_id,
-                                }
+    trainer()
 
-                                loader_path = dataset_to_load
-                                task = task
-                                from_pretrained = models[model]
-                                dropout = dr
-                                hidden_size_multiple = hidden_size
-                                masked_data_collator = masked
-                                print(f'data: {loader_path}, pre-trained: {from_pretrained}, tokenizer: {tokenizer_name}')
-
-                                trainer = TrainModel(
-                                    training_args=TrainingArgumentsTOKCL(**training_args),
-                                    loader_path=loader_path,
-                                    task=task,
-                                    from_pretrained=from_pretrained,
-                                    tokenizer_name=tokenizer_name,
-                                    model_type=model_type,
-                                    do_test=do_test,
-                                    dropout=dropout,
-                                    hidden_size_multiple=hidden_size_multiple,
-                                    masked_data_collator=masked_data_collator,
-                                    file_=test_results_file
-                                )
-
-                                trainer()
-
-                                trainer.save_benchmark_results()
